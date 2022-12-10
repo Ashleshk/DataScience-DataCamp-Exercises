667ea6780003f2bf4869c64b9882b381ab666564
Ashleshk <ashleshuk@gmail.com>
2022-12-09 19:12:41 -0600

    Window Queries
    - Window functions
    - Paritition Function

---
+++ SQL/Data Manipulation in SQL/Window Queries.sql
@@ -0,0 +1,110 @@
+/*
+Select the match ID, country name, season, home, and away goals from the match and country tables.
+Complete the query that calculates the average number of goals scored overall and then includes the aggregate value in each row using a window function.
+*/
+SELECT 
+	-- Select the id, country name, season, home, and away goals
+	m.id, 
+    c.name AS country, 
+    m.season,
+	m.home_goal,
+	m.away_goal,
+    -- Use a window to include the aggregate average in each row
+	AVG(m.home_goal + m.away_goal) over() AS overall_avg
+FROM match AS m
+LEFT JOIN country AS c ON m.country_id = c.id;
+
+
+/*
+Select the league name and average total goals scored from league and match.
+Complete the window function so it calculates the rank of average goals scored across all leagues in the database.
+Order the rank by the average total of home and away goals scored.
+*/
+
+SELECT 
+	-- Select the league name and average goals scored
+	l.name AS league,
+    AVG(m.home_goal + m.away_goal) AS avg_goals,
+    -- Rank each league according to the average goals
+    RANK() OVER(ORDER BY AVG(m.home_goal + m.away_goal)) AS league_rank
+FROM league AS l
+LEFT JOIN match AS m 
+ON l.id = m.country_id
+WHERE m.season = '2011/2012'
+GROUP BY l.name
+-- Order the query by the rank you created
+ORDER BY league_rank;
+
+/*
+Complete the same parts of the query as the previous exercise.
+Complete the window function to rank each league from highest to lowest average goals scored.
+Order the main query by the rank you just created.
+*/
+SELECT 
+	-- Select the league name and average goals scored
+	l.name AS league,
+    AVG(m.home_goal + m.away_goal) AS avg_goals,
+    -- Rank leagues in descending order by average goals
+    RANK() OVER(ORDER BY AVG(m.home_goal + m.away_goal) DESC) AS league_rank
+FROM league AS l
+LEFT JOIN match AS m 
+ON l.id = m.country_id
+WHERE m.season = '2011/2012'
+GROUP BY l.name
+-- Order the query by the rank you created
+ORDER BY league_rank;
+
+
+-----------------------------------------------------------------------------------------
+------                          PARTITION BY
+----------------------------------------------------------------------------------------
+/*
+Complete the two window functions that calculate the home and away goal averages. Partition the window functions by season to calculate separate averages for each season.
+Filter the query to only include matches played by Legia Warszawa, id = 8673.
+*/
+SELECT
+	date,
+	season,
+	home_goal,
+	away_goal,
+	CASE WHEN hometeam_id = 8673 THEN 'home' 
+		 ELSE 'away' END AS warsaw_location,
+    -- Calculate the average goals scored partitioned by season
+    AVG(home_goal) OVER(PARTITION BY season) AS season_homeavg,
+    AVG(away_goal) OVER(PARTITION BY season) AS season_awayavg
+FROM match
+-- Filter the data set for Legia Warszawa matches only
+WHERE 
+	hometeam_id = 8673 
+    OR awayteam_id = 8673
+ORDER BY (home_goal + away_goal) DESC;
+
+
+/*
+Construct two window functions partitioning the average of home and away goals by season and month.
+Filter the dataset by Legia Warszawa's team ID (8673) so that the window calculation only includes matches involving them.
+*/
+SELECT 
+	date,
+	season,
+	home_goal,
+	away_goal,
+	CASE WHEN hometeam_id = 8673 THEN 'home' 
+         ELSE 'away' END AS warsaw_location,
+	-- Calculate average goals partitioned by season and month
+    AVG(home_goal) OVER(PARTITION BY season, 
+         	EXTRACT(month FROM date)) AS season_mo_home,
+    AVG(away_goal) OVER(PARTITION BY season, 
+            EXTRACT(Month FROM date)) AS season_mo_away
+FROM match
+WHERE 
+	hometeam_id = 8673
+    OR awayteam_id = 8673
+ORDER BY (home_goal + away_goal) DESC;
+
+-----------------------------------------------------------------------------------------
+------                         SLIDING BY
+----------------------------------------------------------------------------------------
+
+
+
