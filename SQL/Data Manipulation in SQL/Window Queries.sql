9912cb7c3f806263c61eeeeed7928201c3edf281
Ashleshk <ashleshuk@gmail.com>
2022-12-12 11:35:44 -0600

    window queries

--- SQL/Data Manipulation in SQL/Window Queries.sql
+++ SQL/Data Manipulation in SQL/Window Queries.sql
@@ -105,6 +105,46 @@ ORDER BY (home_goal + away_goal) DESC;
 -----------------------------------------------------------------------------------------
 ------                         SLIDING BY
 ----------------------------------------------------------------------------------------
+/*
+Complete the window function by:
+    Assessing the running total of home goals scored by FC Utrecht.
+    Assessing the running average of home goals scored.
+    Ordering both the running average and running total by date.
+*/
 
+SELECT 
+	date,
+	home_goal,
+	away_goal,
+    -- Create a running total and running average of home goals
+    SUM(home_goal) OVER(ORDER BY date 
+         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS running_total,
+    AVG(home_goal) OVER(ORDER BY date 
+         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS running_avg
+FROM match
+WHERE 
+	hometeam_id = 9908 
+	AND season = '2011/2012';
+
+/*
+Complete the window function by:
+    Assessing the running total of home goals scored by FC Utrecht.
+    Assessing the running average of home goals scored.
+    Ordering both the running average and running total by date, descending.
+*/
+SELECT 
+	-- Select the date, home goal, and away goals
+	date,
+    home_goal,
+    away_goal,
+    -- Create a running total and running average of home goals
+    SUM(home_goal) OVER(ORDER BY date DESC
+         ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) AS running_total,
+    AVG(home_goal) OVER(ORDER BY date DESC
+         ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING) AS running_avg
+FROM match
+WHERE 
+	awayteam_id = 9908 
+    AND season = '2011/2012';
 
 
