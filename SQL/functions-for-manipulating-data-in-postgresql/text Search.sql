eead80107b4a4d193a94a0edb5bdd88aff472ba5
Ashleshk <ashleshuk@gmail.com>
2022-12-13 12:10:45 -0600

    Text Searching

---
+++ SQL/functions-for-manipulating-data-in-postgresql/text Search.sql
@@ -0,0 +1,113 @@
+```Select the film description and convert it to a tsvector data type.
+```
+
+-- Select the film description as a tsvector
+SELECT to_tsvector(description) 
+FROM film;
+
+
+```
+Select the title and description columns from the film table.
+Perform a full-text search on the title column for the word elf.
+```
+
+-- Select the title and description
+SELECT title, description
+FROM film
+-- Convert the title to a tsvector and match it against the tsquery 
+WHERE to_tsvector(title) @@ to_tsquery('elf');
+
+---------------------------------------------------------------------------------------
+--                  USER DEFINED DATA TYPE
+---------------------------------------------------------------------------------------
+
+```Verify that the new data type has been created by looking in the pg_type system table.
+```
+
+-- Create an enumerated data type, compass_position
+CREATE TYPE compass_position AS ENUM (
+  	-- Use the four cardinal directions
+  	'North', 
+  	'South',
+  	'East', 
+  	'West'
+);
+-- Confirm the new data type is in the pg_type system table
+SELECT typname
+FROM pg_type
+WHERE typname='compass_position';
+
+```Select the column_name, data_type, udt_name.
+Filter for the rating column in the film table.```
+-- Select the column name, data type and udt name columns
+SELECT column_name, data_type, udt_name
+FROM INFORMATION_SCHEMA.COLUMNS 
+-- Filter by the rating column in the film table
+WHERE table_name ='film' AND column_name='rating';
+
+
+```inventory_id is currently held by a customer and alias the column as held_by_cust
+
+Now filter your query to only return records where the inventory_held_by_customer() function returns a non-null value.
+```
+-- Select the film title and inventory ids
+SELECT 
+	f.title, 
+    i.inventory_id,
+    -- Determine whether the inventory is held by a customer
+    inventory_held_by_customer(i.inventory_id) as held_by_cust
+FROM film as f 
+	INNER JOIN inventory AS i ON f.film_id=i.film_id 
+WHERE
+	-- Only include results where the held_by_cust is not null
+    inventory_held_by_customer(i.inventory_id) is not null
+
+```Measuring similarity between two strings
+Now that you have enabled the fuzzystrmatch and pg_trgm extensions you can begin to explore their capabilities. First, we will measure the similarity between the title and description from the film table of the Sakila database.
+
+Instructions
+100 XP
+Select the film title and description.
+Calculate the similarity between the title and description.```
+
+
+-- Select the title and description columns
+SELECT 
+  title, 
+  description, 
+  -- Calculate the similarity
+ similarity(title, description)
+FROM 
+  film
+
+```Levenshtein distance examples
+Now let's take a closer look at how we can use the levenshtein function to match strings against text data. If you recall, the levenshtein distance represents the number of edits required to convert one string to another string being compared.
+
+In a search application or when performing data analysis on any data that contains manual user input, you will always want to account for typos or incorrect spellings. The levenshtein function provides a great method for performing this task. In this exercise, we will perform a query against the film table using a search string with a misspelling and use the results from levenshtein to determine a match. Let's check it out.
+
+    Select the film title and film description.
+    Calculate the levenshtein distance for the film title with the string JET NEIGHBOR.```
+
+-- Select the title and description columns
+SELECT  
+  title, 
+  description, 
+  -- Calculate the levenshtein distance
+  levenshtein(title, 'JET NEIGHBOR') AS distance
+FROM 
+  film
+ORDER BY 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
